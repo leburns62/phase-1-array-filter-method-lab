674ffdbf95b5287687e44245cfb16824c825d648
Levi Hutchinson Burns <leburns62@gmail.com>
2022-05-20 15:08:32 -0400

    commit tree 8d0d6e15e1b93fc531f92a5d79c0cb98243d072e

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(driverArray, target) {
+    return driverArray.filter(driver => driver.toUpperCase()
+    === target.toUpperCase())
+}
+
+function fuzzyMatch(drivers, attribute) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().substring(0, attribute.length)
+        === attribute.toLowerCase()
+    })
+}
+
+function matchName(drivers, argument) {
+    return drivers.filter(function(driver) {
+        return driver.name === argument
+    })
+}
